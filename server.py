3d4dae606147999b4310bf5321d2d0e9a2170147
Abhiram V <abhi.raa.man.v@gmail.com>
2022-07-09 22:07:55 +0530

    Move some stuff into functions

--- server.py
+++ server.py
@@ -2,28 +2,37 @@
 
 import socket
 
+BUFFSIZE = 4096
+
 def create_socket():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('', 1234))
     s.listen(5)
     return s
 
-def handle_connection(sock):
-    buf = sock.recv(1024)
+def recvfile(sock, filename):
+    f = open('question', 'wb')
+    buf = sock.recv(BUFFSIZE)
     while len(buf) != 0:
         print(f'received {len(buf)} bytes')
-        print(buf)
-        buf = sock.recv(1024)
+        f.write(buf)
+        #print(buf)
+        buf = sock.recv(BUFFSIZE)
     print('connection closed')
+    print(f'wrote file {filename}')
 
-def main():
+def listen():
     s = create_socket()
+    filename = 'question'
     print('socket created:', s)
-    print('listening...')
+    print('listening')
     while True:
         (clientsocket, addr) = s.accept()
         print('connection accepted:', addr)
-        handle_connection(clientsocket)
+        recvfile(clientsocket, filename)
+
+def main():
+    listen()
 
 if __name__ == '__main__':
     main()
