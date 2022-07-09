bee02f897a73489f3cb07a06d8272b087e780bf6
Abhiram V <abhi.raa.man.v@gmail.com>
2022-07-09 22:09:09 +0530

    Move sending file part into a function send_file
    send_file() sends the file BUFFSIZE bytes at a time and computes the
    message digest.

--- client.py
+++ client.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import socket
+import hashlib
+
+BUFFSIZE = 4096
 
 def connect_server(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip, port))
     return s
+
+    m = hashlib.sha256()
+    m.update(buf)
+    return m.hexdigest()
+
+def send_file(sock, file):
+    if not os.path.isfile(file):
+        print(f'{file} is not a file')
+        return
+
+    f = open(file, 'rb')
+    print(f'sending file {file}')
+    digest = hashlib.sha256()
+    buf = f.read(BUFFSIZE)
+    while len(buf) != 0:
+        digest.update(buf)
+        print(f'sent {sock.send(buf)} bytes')
+        buf = f.read(BUFFSIZE)
+    print(f'hash: {digest.hexdigest()}')
 
 def main():
     if len(sys.argv) < 4:
@@ -14,9 +37,7 @@ def main():
         exit(1)
 
     s = connect_server(sys.argv[1], int(sys.argv[2]))
-    f = open(sys.argv[3], 'rb')
-    print(f'sending file {sys.argv[3]}')
-    print(f'sent {s.send(f.read())} bytes')
+    send_file(s, sys.argv[3])
     s.close()
 
 if __name__ == '__main__':
