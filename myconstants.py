78ea840586829e657194e896be30bc013b3a084f
Abhiram V <abhi.raa.man.v@gmail.com>
2022-07-23 22:12:57 +0530

    Add some defaults for arguments

--- myconstants.py
+++ myconstants.py
@@ -1,6 +1,9 @@
 PORT = 2345
 BUFFSIZE = 4096
-LOGFILE = 'questions_log'
+
+LISTFILE = 'iplist'
+LOGFILE = 'log.csv'
+QDIR = './qns'
 
 # format of time to write to csv file
 # default is iso format date with 24-hour and minutes
