ee210bf75dcd4a6eb70f0cf2b98fb2e480371789
Abhiram V <abhi.raa.man.v@gmail.com>
2022-07-23 22:13:17 +0530

    Decorate output. Also flush on print in some cases
    Flush is required when running from the GUI, since stdout is buffered,
    but not stderr, and so the output may not be in order.

--- client.py
+++ client.py
@@ -38,7 +38,6 @@ def connect_server(ip, port):
 '''
 def logwrite(logfile, logdict):
     try:
-        print('writing log')
         qnfp = open(logdict['qpath'], 'r')
         log = {
             'Time': [time.strftime(myconstants.TIMEFMT)],
@@ -47,9 +46,7 @@ def logwrite(logfile, logdict):
             'Question Head': [qnfp.readline().strip()],
             'Hash': [logdict['hash']]
         }
-        print(log)
         df = pd.DataFrame(log)
-        print('logfile:', logfile)
         f = open(logfile, 'a')
         size = f.tell()
         f.close()
@@ -58,22 +55,22 @@ def logwrite(logfile, logdict):
         #            f"question_head: {qnfp.readline().strip()} "
         #            f"hash: {logdict['hash']}\n")
     except Exception as arg:
-        print('exception writing log:', arg)
+        printerr('exception writing log:', arg)
 
 def send_file(sock, file):
     if not os.path.isfile(file):
-        print(f'{file} is not a file')
+        printerr(f'{file} is not a file')
         return
 
     f = open(file, 'rb')
-    print(f'sending file {file}')
+    print(f'\tsending file {file}', flush=True)
     digest = hashlib.sha256()
     buf = f.read(myconstants.BUFFSIZE)
     while len(buf) != 0:
         digest.update(buf)
-        print(f'sent {sock.send(buf)} bytes')
+        print(f'\tsent {sock.send(buf)} bytes', flush=True)
         buf = f.read(myconstants.BUFFSIZE)
-    print(f'hash: {digest.hexdigest()}')
+    print(f'\thash: {digest.hexdigest()}', flush=True)
     return digest.hexdigest()
 
 def send_questions(order, port, qdir, logfile=myconstants.LOGFILE, verbose=False):
@@ -87,17 +84,15 @@ def send_questions(order, port, qdir, logfile=myconstants.LOGFILE, verbose=False
     for i in order.keys():
         try:
             s = connect_server(i, port)
-            print(f'connected to {i}:{port}')
+            print(f'[+] connected to {i}:{port}', flush=True)
             qpath = os.path.join(qdir, order[i])
-            print('\tsending file', qpath)
             filesig = send_file(s, qpath)
             # todo: write log as a csv file
             logdict = {'sys': i, 'q': order[i], 'hash': filesig, 'qpath': qpath}
             logwrite(logfile, logdict)
             s.close()
-            print('closed connection')
         except:
-            printerr(f'connection to {i}:{port} failed')
+            printerr(f'[*] connection to {i}:{port} failed')
 
 def argparser():
     parser = argparse.ArgumentParser()
@@ -123,10 +118,10 @@ def argparser():
         sys.exit(1)
 
     if args.verbose:
-        print('[-] reading IPs/hostname from file:', args.list)
-        print('[-] questions directory:', args.questions_dir)
-        print('[-] destination port:', args.port)
-        print('[-] log file:', args.logfile)
+        print('[-] reading IPs/hostname from file:', args.list,
+              '\n[-] questions directory:', args.questions_dir,
+              '\n[-] destination port:', args.port,
+              '\n[-] log file:', args.logfile, flush=True)
 
     return args
 
