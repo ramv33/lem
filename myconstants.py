e65e882533cdd91aff39a1330988991efc232a2e
Abhiram V <abhi.raa.man.v@gmail.com>
2022-07-24 11:51:53 +0530

    Add defaults for mongo connection

--- myconstants.py
+++ myconstants.py
@@ -5,6 +5,11 @@ LISTFILE = 'iplist'
 LOGFILE = 'log.csv'
 QDIR = './qns'
 
+# Mongo stuff
+MONGO_URL='mongodb://127.0.0.1:27017/'
+DBNAME='LMS'
+COLLECTION_NAME='info'
+
 # format of time to write to csv file
 # default is iso format date with 24-hour and minutes
 TIMEFMT = '%Y-%m-%d %H:%M'
